93cea3fe43fe8f4d6f3242be017c65f240f7928b
ksenyshutova <shutovasn@mail.ru>
2024-03-21 20:14:59 +0300

    Первая версия диплома

---
+++ public/homePage.js
@@ -0,0 +1,112 @@
+"use strict";
+
+let userLogout = new LogoutButton();
+userLogout.action = () =>
+ApiConnector.logout(response => {
+    console.log(response);
+    if (response.success) {
+        location.reload();
+    }
+});
+
+ApiConnector.current(response => {
+    console.log(response);
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+let userRatesBoard = new RatesBoard();
+ApiConnector.getStocks(response => {
+    console.log(response);
+    if (response.success) {
+        userRatesBoard.clearTable();
+        userRatesBoard.fillTable(response.data);
+    }
+});
+    setInterval(ApiConnector.getStocks(response => {
+    console.log(response);
+    if (response.success) {
+        userRatesBoard.clearTable();
+        userRatesBoard.fillTable(response.data);
+    }
+    }), 60000);
+
+
+let userMoneyManager = new MoneyManager();
+userMoneyManager.addMoneyCallback = data => {
+    console.log(data);
+ApiConnector.addMoney(data, response => {
+        console.log(response);
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        userMoneyManager.setMessage(response.success, response.message = 'Пополнение счёта успешно выполнено');
+    } else {
+    userMoneyManager.setMessage(response.success, response.message = response.error);
+    }
+});
+};
+
+userMoneyManager.conversionMoneyCallback = data => {
+    console.log(data);
+    ApiConnector.convertMoney(data, response => {
+        console.log(response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            userMoneyManager.setMessage(response.success, response.message = 'Конвертация валют успешно выполнена');
+        } else {
+            userMoneyManager.setMessage(response.success, response.message = response.error);
+            }
+    });
+};
+
+userMoneyManager.sendMoneyCallback = data => {
+    console.log(data);
+ApiConnector.transferMoney(data, response => {
+    console.log(response);
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+userMoneyManager.setMessage(response.success, response.message = 'Перевод средств успешно выполнен');
+} else {
+    userMoneyManager.setMessage(response.success, response.message = response.error);
+    }
+});
+}
+
+let userFavoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+    console.log(response);
+    if (response.success) {
+        userFavoritesWidget.clearTable();
+        userFavoritesWidget.fillTable(response.data);
+        userMoneyManager.updateUsersList(response.data);
+    }
+});
+
+userFavoritesWidget.addUserCallback = data => {
+    console.log(data);
+    ApiConnector.addUserToFavorites(data, response => {
+        console.log(response);
+       if (response.success) {
+        userFavoritesWidget.clearTable();
+        userFavoritesWidget.fillTable(response.data);
+        userMoneyManager.updateUsersList(response.data);
+} else {
+    userMoneyManager.setMessage(response.success, response.message = response.error);
+    }
+    });
+}
+
+userFavoritesWidget.removeUserCallback = data => {
+    console.log(data);
+    ApiConnector.removeUserFromFavorites(data, response => {
+        console.log(response);
+        if (response.success) {
+        userFavoritesWidget.clearTable();
+        userFavoritesWidget.fillTable(response.data);
+        userMoneyManager.updateUsersList(response.data);
+} else {
+    userMoneyManager.setMessage(response.success, response.message = response.error);
+    }
+    });
+}
---
+++ public/loginPage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let userForm = new UserForm();
+userForm.loginFormCallback = data => {
+    console.log(data);
+ApiConnector.login(data, response => {
+    console.log(response);
+    if (response.success) {
+        location.reload();
+    } else {
+    userForm.setLoginErrorMessage('Ошибка в логине или пароле, повторите попытку');
+    }
+});
+};
+
+userForm.registerFormCallback = data => {
+    console.log(data);
+ApiConnector.register(data, response => {
+    console.log(response);
+    if (response.success) {
+        location.reload();
+    } else {
+        userForm.setRegisterErrorMessage('Ошибка в регистрации, повторите попытку');
+    }
+});
+};
+
