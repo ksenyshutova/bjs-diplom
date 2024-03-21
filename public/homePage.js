1c11bfcd67acbb5678846a6e67effec88ea5d553
ksenyshutova <shutovasn@mail.ru>
2024-03-21 20:17:37 +0300

    1ая версия диплома

--- public/homePage.js
+++ public/homePage.js
@@ -2,12 +2,12 @@
 
 let userLogout = new LogoutButton();
 userLogout.action = () =>
-ApiConnector.logout(response => {
-    console.log(response);
-    if (response.success) {
-        location.reload();
-    }
-});
+    ApiConnector.logout(response => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        }
+    });
 
 ApiConnector.current(response => {
     console.log(response);
@@ -24,27 +24,27 @@ ApiConnector.getStocks(response => {
         userRatesBoard.fillTable(response.data);
     }
 });
-    setInterval(ApiConnector.getStocks(response => {
+setInterval(ApiConnector.getStocks(response => {
     console.log(response);
     if (response.success) {
         userRatesBoard.clearTable();
         userRatesBoard.fillTable(response.data);
     }
-    }), 60000);
+}), 60000);
 
 
 let userMoneyManager = new MoneyManager();
 userMoneyManager.addMoneyCallback = data => {
     console.log(data);
-ApiConnector.addMoney(data, response => {
+    ApiConnector.addMoney(data, response => {
         console.log(response);
-    if (response.success) {
-        ProfileWidget.showProfile(response.data);
-        userMoneyManager.setMessage(response.success, response.message = 'Пополнение счёта успешно выполнено');
-    } else {
-    userMoneyManager.setMessage(response.success, response.message = response.error);
-    }
-});
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            userMoneyManager.setMessage(response.success, response.message = 'Пополнение счёта успешно выполнено');
+        } else {
+            userMoneyManager.setMessage(response.success, response.message = response.error);
+        }
+    });
 };
 
 userMoneyManager.conversionMoneyCallback = data => {
@@ -56,21 +56,21 @@ userMoneyManager.conversionMoneyCallback = data => {
             userMoneyManager.setMessage(response.success, response.message = 'Конвертация валют успешно выполнена');
         } else {
             userMoneyManager.setMessage(response.success, response.message = response.error);
-            }
+        }
     });
 };
 
 userMoneyManager.sendMoneyCallback = data => {
     console.log(data);
-ApiConnector.transferMoney(data, response => {
-    console.log(response);
-    if (response.success) {
-        ProfileWidget.showProfile(response.data);
-userMoneyManager.setMessage(response.success, response.message = 'Перевод средств успешно выполнен');
-} else {
-    userMoneyManager.setMessage(response.success, response.message = response.error);
-    }
-});
+    ApiConnector.transferMoney(data, response => {
+        console.log(response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            userMoneyManager.setMessage(response.success, response.message = 'Перевод средств успешно выполнен');
+        } else {
+            userMoneyManager.setMessage(response.success, response.message = response.error);
+        }
+    });
 }
 
 let userFavoritesWidget = new FavoritesWidget();
@@ -87,13 +87,13 @@ userFavoritesWidget.addUserCallback = data => {
     console.log(data);
     ApiConnector.addUserToFavorites(data, response => {
         console.log(response);
-       if (response.success) {
-        userFavoritesWidget.clearTable();
-        userFavoritesWidget.fillTable(response.data);
-        userMoneyManager.updateUsersList(response.data);
-} else {
-    userMoneyManager.setMessage(response.success, response.message = response.error);
-    }
+        if (response.success) {
+            userFavoritesWidget.clearTable();
+            userFavoritesWidget.fillTable(response.data);
+            userMoneyManager.updateUsersList(response.data);
+        } else {
+            userMoneyManager.setMessage(response.success, response.message = response.error);
+        }
     });
 }
 
@@ -102,11 +102,11 @@ userFavoritesWidget.removeUserCallback = data => {
     ApiConnector.removeUserFromFavorites(data, response => {
         console.log(response);
         if (response.success) {
-        userFavoritesWidget.clearTable();
-        userFavoritesWidget.fillTable(response.data);
-        userMoneyManager.updateUsersList(response.data);
-} else {
-    userMoneyManager.setMessage(response.success, response.message = response.error);
-    }
+            userFavoritesWidget.clearTable();
+            userFavoritesWidget.fillTable(response.data);
+            userMoneyManager.updateUsersList(response.data);
+        } else {
+            userMoneyManager.setMessage(response.success, response.message = response.error);
+        }
     });
 }
--- public/loginPage.js
+++ public/loginPage.js
@@ -3,25 +3,25 @@
 let userForm = new UserForm();
 userForm.loginFormCallback = data => {
     console.log(data);
-ApiConnector.login(data, response => {
-    console.log(response);
-    if (response.success) {
-        location.reload();
-    } else {
-    userForm.setLoginErrorMessage('Ошибка в логине или пароле, повторите попытку');
-    }
-});
+    ApiConnector.login(data, response => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage('Ошибка в логине или пароле, повторите попытку');
+        }
+    });
 };
 
 userForm.registerFormCallback = data => {
     console.log(data);
-ApiConnector.register(data, response => {
-    console.log(response);
-    if (response.success) {
-        location.reload();
-    } else {
-        userForm.setRegisterErrorMessage('Ошибка в регистрации, повторите попытку');
-    }
-});
+    ApiConnector.register(data, response => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage('Ошибка в регистрации, повторите попытку');
+        }
+    });
 };
 
